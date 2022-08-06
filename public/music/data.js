ace836d96ed2671d9b7c92a91d616151ce88e143
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2022-08-06 14:35:56 +0000

    git commit

---
+++ public/music/data.js
@@ -0,0 +1,52 @@
+	var data=new Array();//Data数据数组
+		data[0]={
+			title: "After Tomorrow",
+			text: "Neateasy Games",
+			id: "1329466574",
+			img:"https://luspecial.vercel.app/background.jpg",
+			type:"游戏;精选;高级"}
+		data[1]={
+			title: "花与舞",
+			text:"Flower Dance",
+			id:"552194857",
+			img: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"}
+		data[2]={
+			title: "亡灵序曲",
+			text:"Intro:The Dawn 李嘉诚(Jacong)",
+			id:"1467730592",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+		data[3]={
+			title: "优雅嘲讽(原曲Groove Battle)",
+			text:"Up主圆周率在地球上已留学多年",
+			id:"1957256466",
+			img: "https://t15.baidu.com/it/u=2558305100,3869169614&fm=220&app=103&size=f558,372&n=0&f=JPEG&fmt=auto&maxorilen2heic=2000000"}
+		data[4]={
+			title: "Tennessee",
+			text:"点击播放",
+			id:"568888000",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+		data[5]={
+			title: "Beyond The Memory",
+			text:"点击播放",
+			id:"5307982",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+		data[6]={
+			title: "オラリオン",
+			text:"点击播放",
+			mid:"https://luspecial.vercel.app/cloud/file/private/musicZjdcrts2-2.mp3",
+			img: "https://i0.hdslb.com/bfs/bangumi/34a46a3d1504d05febaa14d0b5174015649974df.jpg@524w_698h.webp"}
+		data[7]={
+			title: "营地中心",
+			text:"Neateasy Games Background Music",
+			id:"1329466573",
+			img: "https://mrzh.res.netease.com/pc/fab/20181019110746/img/bg_05_2_196d3bf.jpg"}
+		data[8]={
+			title: "晨曦酒庄",
+			text:"Mihoyo Games Background Music",
+			id:"1455706951",
+			img: "https://webstatic.mihoyo.com/upload/contentweb/2022/07/24/3c16216d6e7f1dc788ba783d3a793d9e_8358466801763713902.jpg"}
+		data[9]={
+			title: "钢铁洪流进行曲",
+			text:"国庆70周年庆典背景音乐之一，国创",
+			id:"1394369908",
+			img: "https://gimg3.baidu.com/search/src=https%3A%2F%2Fbkimg.cdn.bcebos.com%2Fsmart%2F3ac79f3df8dcd100baa1876012c15010b912c8fc8112-bkimg-process%2Cv_1%2Crw_1%2Crh_1%2Cmaxl_300%2Cpad_1&refer=http%3A%2F%2Fwww.baidu.com&app=2021&size=w348&n=0&g=0n&er=404&q=75&fmt=auto&maxorilen2heic=2000000"}
