227e11cde4a86435228c0fe3f68025a16328828f
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2022-08-12 13:21:09 +0000

    update public/music/data.js.
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

--- public/music/data.js
+++ public/music/data.js
@@ -39,7 +39,7 @@
 			title: "营地中心",
 			text:"Neateasy Games Background Music",
 			id:"1329466573",
-			img: "https://mrzh.res.netease.com/pc/fab/20181019110746/img/bg_05_2_196d3bf.jpg"}
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/ef19750eee8e8d4f50e841cc390ccd02fcab5bdf30495623655b4b7ecdcd945b45f6eaa770cb5717d4f956801e38963a?pictype=scale&from=30113&version=3.3.3.3&fname=R-C.jpg&size=750"}
 		data[8]={
 			title: "晨曦酒庄",
 			text:"Mihoyo Games Background Music",
@@ -47,34 +47,34 @@
 			img: "https://webstatic.mihoyo.com/upload/contentweb/2022/07/24/3c16216d6e7f1dc788ba783d3a793d9e_8358466801763713902.jpg"}
 		data[9]={
 			title: "钢铁洪流进行曲",
-			text:"国庆70周年庆典背景音乐之一，国创",
+			text:"国庆70周年庆典背景音乐之一,著名音乐",
 			id:"1394369908",
 			img: "https://gimg3.baidu.com/search/src=https%3A%2F%2Fbkimg.cdn.bcebos.com%2Fsmart%2F3ac79f3df8dcd100baa1876012c15010b912c8fc8112-bkimg-process%2Cv_1%2Crw_1%2Crh_1%2Cmaxl_300%2Cpad_1&refer=http%3A%2F%2Fwww.baidu.com&app=2021&size=w348&n=0&g=0n&er=404&q=75&fmt=auto&maxorilen2heic=2000000"}
         data[10]={
             title: "aLize",text: "一首燃曲，澤野弘之作之",
-            id: "1313052233",img: ""}
+            id: "1313052233",img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
         data[11]={
             title: "千本桜",text: "歌手：黒うさP / 初音ミク",
-            id: "26096272",img: ""}
+            id: "26096272",img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
         data[12]={
             title: "Never Gonna Give You Up",text: "你被骗了，但是自愿。<br>维诺.理查德.汪苏泷",
-            id:"5221167",img:""}
+            id:"5221167",img:"https://njc-download.weiyun.com/ftn_handler/1d57ea3c9a3cf4908ac68b9a07455f5704ff1eff1c6deee93bcb74aa7d5494eef7b8efafe71e387ccd03395d0ba508c75b842798c00fe9a106dfe6c9b70a5181/NGGYU.jpg?fname=NGGYU.jpg&from=30113&version=3.3.3.3"}
         data[13]={
             title: "星天(Star Sky)",text:"常用战忽局BGM,燃起来了！",
-            id: "31654478",img:""}
+            id: "31654478",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
         data[14]={
             title: "Astronomia",text:"emmmm,慎听",
             id: "430114655",img:"https://cn.bing.com/images/search?q=%e9%bb%91%e4%ba%ba%e6%8a%ac%e6%a3%ba&id=72936BCE48F6A37AAD2F99B27806DBF40A14C413&FORM=IQFRBA&tsc=ImageHoverTitle"}
         data[15]={
             title:"",text:"平静的小曲，网友神评：“听着这音乐打代码，感觉我在拯救世界！！！”",
-            id:"2112355",img:""}
+            id:"2112355",img:"https://gzc-download.weiyun.com/ftn_handler/a044f433374fe0398b0e25c38dac068c43339db3388b6e68d9d4aa49ce638332bcc176252ee4829ec054bc62c5d570ea370e872a88461d27807a1850d4e0f953/DMKJ.jpg?fname=DMKJ.jpg&from=30113&version=3.3.3.3"}
         data[16]={
             title:"苏维埃进行曲",text:"不知道该怎么描述",
-            id:"5042951",img:""}
+            id:"5042951",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
         data[17]={
             title:"So far away",text:"网易云音乐电音榜单",
-            id:"521416693",img:""}
+            id:"521416693",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
         data[18]={
-            title:"<h4 style='color:red'>MV</h4>白天不懂夜的黑",text:"“年轻人不要太气盛”“不气盛叫年轻人吗”“是龙得盘着，是虎得卧着”“刘华强，我告诉你，你今天要是敢踏出这房门一步...”“痛快，我就喜欢你这脾气”[doge]",
-            source:"https://player.bilibili.com/player.html?aid=91073180&bvid=BV11j411f7ak&cid=155517433&page=1",img:""
+            title:"<h4 style='color:red'>MV</h4>白天不懂夜的黑",text:"嗯...这图片你敢直接看吗？！“年轻人不要太气盛”“不气盛叫年轻人吗”“是龙得盘着，是虎得卧着”“刘华强，我告诉你，你今天要是敢踏出这房门一步...”“痛快，我就喜欢你这脾气”[doge]",
+            source:"https://player.bilibili.com/player.html?aid=91073180&bvid=BV11j411f7ak&cid=155517433&page=1",img:"https://tjc-download.weiyun.com/ftn_handler/5a2800928144781f5b6f53598e4c6d998ad2965c9a68416461d758c71db93237964b7d682c8d41bf444ce3c9555ef6b6abaadaca144fba0517998d75024035bb/v2-b869d0b7668b2cb7f73c90bf252dede6_r.jpg?fname=v2-b869d0b7668b2cb7f73c90bf252dede6_r.jpg&from=30113&version=3.3.3.3"
         }
