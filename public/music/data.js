e1b313fe1c006c92bb01b463982bfe84d08029f0
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2022-09-12 03:50:00 +0000

    update public/music/data.js.
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

--- public/music/data.js
+++ public/music/data.js
@@ -1,7 +1,7 @@
 	var data=new Array();//Data数据数组
 		data[0]={
 			title: "After Tomorrow",
-			text: "网易游戏背景音乐",
+			text: "网易游戏特辑音乐",
 			id: "1329466574",
 			img:"https://luspecial.vercel.app/background.jpg",
 			type:"游戏;精选;高级"}
