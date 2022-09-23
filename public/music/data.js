b08bf1bbf2bd37a8743afacf083416724b074113
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2022-09-23 01:30:02 +0000

    update public/music/data.js.
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

--- public/music/data.js
+++ public/music/data.js
@@ -111,7 +111,7 @@
             title:"<h4 style='color:red'>MV</h4>永远不会放弃您(Never Gonna Give You Up 豪华版)",text:"您要被骗了！",source:"https://player.bilibili.com/player.html?aid=601888108&bvid=BV1vB4y1574y&cid=802251467&page=1",img:"https://www.concierto.cl/wp-content/uploads/2021/02/Rick-Astley.jpg"
         }
         data[30]={
-            title:"斩雾破竹",text:"",id:"1879108765",img:""
+            title:"空行不羁",text:"压迫感就此显现",id:"1879108774",img:""
         }
         data[31]={
             title:"His theme",text:"音乐源于传说之下",id:"39227633",img:""
